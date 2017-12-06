2ae20012ab8d19460ff6276622aefc9f086f946f
Douglas Linder <douglas.linder@gmail.com>
2017-12-06 22:27:06 +0800

    more tests

---
+++ RogerRoger.Tests/Helpers/MockRejectContent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using RogerRoger.IO;
+
+namespace RogerRoger.Tests.Helpers
+{
+  public class MockRejectContent : IFileProcessTask
+  {
+    private readonly string _badContent;
+
+    public MockRejectContent(string badContent)
+    {
+      _badContent = badContent;
+    }
+
+    public Task<string> Process(string input)
+    {
+      if (input == _badContent) throw new Exception("Invalid content");
+      return Task.Run(() => input);
+    }
+  }
+}
--- RogerRoger.Tests/TestFileTransformer.cs
+++ RogerRoger.Tests/TestFileTransformer.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Data;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Moq;
 using NLog;
 using RogerRoger.IO;
 using RogerRoger.Tests.Fixtures;
+using RogerRoger.Tests.Helpers;
 using Xunit;
 
 namespace RogerRoger.Tests
@@ -38,6 +40,40 @@ namespace RogerRoger.Tests
       await transformer.Transform(@"C:\home\foo\tests", action);
 
       Assert.Equal(6, collector.Count);
+    }
+
+    [Fact]
+    public async Task TestMatch()
+    {
+      var fs = MockFileSystem.MockFileSystemSimple();
+      var logger = new Mock<ILogger>();
+      var transformer = new FileTransformer(fs, logger.Object);
+      var collector = new ConcurrentBag<string>();
+      var action = new FileAction(fs, (path) => path.ToLowerInvariant().EndsWith(".sql"), (path) => { collector.Add(path); }, (_) => { });
+
+      await transformer.Transform(@"C:\home\foo\tests", action);
+
+      Assert.Equal(4, collector.Count);
+    }
+
+    [Fact]
+    public async Task TestFailed()
+    {
+      var fs = MockFileSystem.MockFileSystemSimple();
+      var logger = new Mock<ILogger>();
+      var transformer = new FileTransformer(fs, logger.Object);
+      var success = new ConcurrentBag<string>();
+      var reject = new ConcurrentBag<string>();
+      var action = new FileAction(
+          fs,
+          (path) => path.ToLowerInvariant().EndsWith(".sql"),
+          (path) => { success.Add(path); }, (path) => { reject.Add(path); })
+        .Add(new MockRejectContent("..."));
+
+      await transformer.Transform(@"C:\home\foo\tests", action);
+
+      Assert.Equal(2, success.Count);
+      Assert.Equal(2, reject.Count);
     }
   }
 }
